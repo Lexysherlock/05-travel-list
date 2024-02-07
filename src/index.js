0566a8dc8c5363f23cf6caaff5d146a73b4f57c5
Lexysherlock <nmimthath18@gmail.com>
2024-02-07 19:46:41 +0530

    updated all the files to git

--- src/index.js
+++ src/index.js
@@ -1,17 +1,13 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import { createRoot } from "react-dom/client"; // Importing createRoot from react-dom/client
+import React from "react"; // Importing React is necessary for JSX
+import "./index.css";
+import App from "./App";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = createRoot(document.getElementById("root")); // Using createRoot from react-dom/client
 root.render(
   <React.StrictMode>
+    {" "}
+    {/* Ensure React is imported */}
     <App />
   </React.StrictMode>
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
